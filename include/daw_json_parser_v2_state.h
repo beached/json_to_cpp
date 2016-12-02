fe3012e6dba1efa604735d1eaaac02762fea342f
Darrell Wright <dwright-git@dawdevel.ca>
2016-12-01 21:41:19 -0500

    .

--- include/daw_json_parser_v2_state.h
+++ include/daw_json_parser_v2_state.h
@@ -1,4 +1,3 @@
-
 // The MIT License (MIT)
 //
 // Copyright (c) 2016 Darrell Wright
@@ -25,6 +24,7 @@
 
 #include <boost/utility/string_view.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/utility/string_view.hpp>
 #include <string>
 #include <vector>
 
@@ -33,8 +33,6 @@
 namespace daw {
 	namespace json {
 		namespace state {
-			enum class current_state_t { none = 0, in_object_name, in_object_value, in_array, current_state_t_size };
-
 			struct state_t {
 				state_t( ) = default;
 				state_t( state_t const & ) = default;
@@ -53,24 +51,77 @@ namespace daw {
 				virtual void on_real( boost::string_view );
 				virtual void on_boolean( bool ); 
 				virtual void on_null( );
-				virtual std::string to_string( ) const = 0;
 			};	// state_t
 
-			struct state_in_object_name_t: public state_t {
-				state_in_object_name_t( ) = default;
+			template<typename CBOnString, typename CBOnObjEnd>
+			class state_in_object_name_t final: public state_t {
+				CBOnString m_cb_on_string;
+				CBOnObjEnd m_cb_on_object_end;
+			public:
+				state_in_object_name_t( ):
+						state_t{ },
+						m_cb_on_string{ },
+						m_cb_on_object_end{ } { }
+
+				state_in_object_name_t( CBOnString cb_on_string, CBOnObjEnd cb_on_object_end ):
+						state_t{ },
+						m_cb_on_string{ std::move( cb_on_string ) },
+						m_cb_on_object_end{ std::move( cb_on_object_end ) } { }
+
 				state_in_object_name_t( state_in_object_name_t const & ) = default;
 				state_in_object_name_t( state_in_object_name_t && ) = default;
 				state_in_object_name_t & operator=( state_in_object_name_t const & ) = default;
 				state_in_object_name_t & operator=( state_in_object_name_t && ) = default;
 
 				~state_in_object_name_t( );
-				std::string to_string( ) const override;
-				void on_string( boost::string_view value ) override;
-				void on_object_end( ) override;
+
+				void on_string( boost::string_view value ) override {
+					m_cb_on_string( value );
+				}
+
+				void on_object_end( ) override {
+					m_cb_on_object_end( );
+				}
 			};	// state_in_object_name
 
-			struct state_in_object_value_t: public state_t {
-				state_in_object_value_t( ) = default;
+			template<typename CBOnString, typename CBOnObjEnd>
+			state_in_object_name_t::~state_in_object_name_t( ) { }
+
+			template<typename CBOnString, typename CBOnObjEnd>
+			auto make_state_in_object_name_t( CBOnString cb_on_string, CBOnObjEnd cb_on_object_end ) {
+				return state_in_object_name_t<CBOnString, CBOnObjEnd>{ std::move( cb_on_string ), std::move( cb_on_object_end ) };
+			}
+
+			template<typename CBOnObjBegin, typename CBOnAryBegin, typename CBOnNull, typename CBOnIntegral, typename CBOnReal, typename CBOnString, typename CBOnBoolean>
+			struct state_in_object_value_t final: public state_t {
+				CBOnObjBegin m_cb_on_object_begin; 
+				CBOnAryBegin m_cb_on_array_begin; 
+				CBOnNull m_cb_on_null; 
+				CBOnIntegral m_cb_on_integral; 
+				CBOnReal m_cb_on_real; 
+				CBOnString m_cb_on_string; 
+				CBOnBoolean m_cb_on_boolean;
+
+				state_in_object_value_t( ):
+						state_t{ },
+						m_cb_on_object_begin{ },
+						m_cb_on_array_begin{ }, 
+						m_cb_on_null{ }, 
+						m_cb_on_integral{ }, 
+						m_cb_on_real{ }, 
+						m_cb_on_string{ }, 
+						m_cb_on_boolean{ } { }
+
+				state_in_object_value_t( CBOnObjBegin cb_on_object_begin, CBOnAryBegin cb_on_array_begin, CBOnNull cb_on_null, CBOnIntegral cb_on_integral, CBOnReal cb_on_real, CBOnString cb_on_string, CBOnBoolean cb_on_boolean ):
+						state_t{ },
+						m_cb_on_object_begin{ std::move( cb_on_object_begin ) },
+						m_cb_on_array_begin{ std::move( cb_on_array_begin ) }, 
+						m_cb_on_null{ std::move( cb_on_null ) }, 
+						m_cb_on_integral{ std::move( cb_on_integral ) }, 
+						m_cb_on_real{ std::move( cb_on_real ) }, 
+						m_cb_on_string{ std::move( cb_on_string ) }, 
+						m_cb_on_boolean{ std::move( cb_on_boolean ) } { }
+
 				state_in_object_value_t( state_in_object_value_t const & ) = default;
 				state_in_object_value_t( state_in_object_value_t && ) = default;
 				state_in_object_value_t & operator=( state_in_object_value_t const & ) = default;
@@ -78,19 +129,90 @@ namespace daw {
 
 				~state_in_object_value_t( );
 
-				std::string to_string( ) const override;
-				void on_object_begin( ) override;
-				void on_array_begin( ) override;
-				void on_null( ) override;
+				void on_object_begin( ) override {
+					m_cb_on_object_begin( );
+				}
 
-				void on_integral( boost::string_view value ) override;
-				void on_real( boost::string_view value ) override;
-				void on_string( boost::string_view value ) override;
-				void on_boolean( bool value ) override;
+				void on_array_begin( ) override {
+					m_cb_on_array_begin( );
+				}
+
+				void on_null( ) override {
+					m_cb_on_null( );
+				}
+
+				void on_integral( boost::string_view value ) override {
+					m_cb_on_integral( value );
+				}
+
+				void on_real( boost::string_view value ) override {
+					m_cb_on_real( value );
+				}
+
+				void on_string( boost::string_view value ) override {
+					m_cb_on_string( value );
+				}
+
+				void on_boolean( bool value ) override {
+					m_cb_on_boolean( value );
+				}
 			};	// state_in_object_value_t
 
+			template<typename CBOnObjBegin, typename CBOnAryBegin, typename CBOnNull, typename CBOnIntegral, typename CBOnReal, typename CBOnString, typename CBOnBoolean>
+			state_in_object_value::~state_in_object_value_t( ) { }
+
+			template<typename CBOnObjBegin, typename CBOnAryBegin, typename CBOnNull, typename CBOnIntegral, typename CBOnReal, typename CBOnString, typename CBOnBoolean>
+			auto make_state_in_object_value_t( CBOnObjBegin cb_on_object_begin, CBOnAryBegin cb_on_array_begin, CBOnNull cb_on_null, CBOnIntegral cb_on_integral, CBOnReal cb_on_real, CBOnString cb_on_string, CBOnBoolean cb_on_boolean ) {
+				return state_in_object_value_t<
+							CBOnObjBegin, 
+							CBOnAryBegin, 
+							CBOnNull, 
+							CBOnIntegral, 
+							CBOnReal, 
+							CBOnString, 
+							CBOnBoolean>{ std::move( cb_on_object_begin ), 
+									std::move( cb_on_array_begin ), 
+									std::move( cb_on_null ), 
+									std::move( cb_on_integral ),
+									std::move( cb_on_real ),
+									std::move( cb_on_string ),
+									std::move( cb_on_boolean };
+			}
+
+			template<typename CBOnObjBegin, typename CBOnAryBegin, typename CBOnAryEnd, typename CBOnNull, typename CBOnIntegral, typename CBOnReal, typename CBOnString, typename CBOnBoolean>
 			struct state_in_array_t: public state_t {
-				state_in_array_t( ) = default;
+				CBOnObjBegin m_cb_on_object_begin; 
+				CBOnAryBegin m_cb_on_array_begin; 
+				CBOnAryEnd m_cb_on_array_end; 
+				CBOnNull m_cb_on_null; 
+				CBOnIntegral m_cb_on_integral; 
+				CBOnReal m_cb_on_real; 
+				CBOnString m_cb_on_string; 
+				CBOnBoolean m_cb_on_boolean;
+
+				state_in_array_t( ):
+						state_t{ },
+						m_cb_on_object_begin{ },
+						m_cb_on_array_begin{ }, 
+						m_cb_on_array_end{ }, 
+						m_cb_on_null{ }, 
+						m_cb_on_integral{ }, 
+						m_cb_on_real{ }, 
+						m_cb_on_string{ }, 
+						m_cb_on_boolean{ } { }
+
+				state_in_array_t( CBOnObjBegin cb_on_object_begin, CBOnAryBegin cb_on_array_begin, CBOnAryEnd cb_on_array_end, CBOnNull cb_on_null, CBOnIntegral cb_on_integral, CBOnReal cb_on_real, CBOnString cb_on_string, CBOnBoolean cb_on_boolean ):
+						state_t{ },
+						m_cb_on_object_begin{ std::move( cb_on_object_begin ) },
+						m_cb_on_array_begin{ std::move( cb_on_array_begin ) }, 
+						m_cb_on_array_end{ std::move( cb_on_array_end ) }, 
+						m_cb_on_null{ std::move( cb_on_null ) }, 
+						m_cb_on_integral{ std::move( cb_on_integral ) }, 
+						m_cb_on_real{ std::move( cb_on_real ) }, 
+						m_cb_on_string{ std::move( cb_on_string ) }, 
+						m_cb_on_boolean{ std::move( cb_on_boolean ) } { }
+
+
 				state_in_array_t( state_in_array_t const & ) = default;
 				state_in_array_t( state_in_array_t && ) = default;
 				state_in_array_t & operator=( state_in_array_t const & ) = default;
@@ -98,19 +220,77 @@ namespace daw {
 
 				~state_in_array_t( );
 
-				std::string to_string( ) const override;
-				void on_object_begin( ) override;
-				void on_array_begin( ) override;
-				void on_array_end( ) override;
-				void on_null( ) override;
-				void on_integral( boost::string_view value ) override;
-				void on_real( boost::string_view value ) override;
-				void on_string( boost::string_view value ) override;
-				void on_boolean( bool value ) override;
+				void on_object_begin( ) override {
+					m_cb_on_object_begin( );
+				}
+
+				void on_array_begin( ) override {
+					m_cb_on_array_begin( );
+				}
+
+				void on_array_end( ) override {
+					m_cb_on_array_end( );
+				}
+
+				void on_null( ) override {
+					m_cb_on_null( );
+				}
+
+				void on_integral( boost::string_view value ) override {
+					m_cb_on_integral( value );
+				}
+
+				void on_real( boost::string_view value ) override {
+					m_cb_on_real( value );
+				}
+
+				void on_string( boost::string_view value ) override {
+					m_cb_on_string( value );
+				}
+
+				void on_boolean( bool value ) override {
+					m_cb_on_boolean( value );
+				}
 			};	// state_in_array_t
 
+			template<typename CBOnObjBegin, typename CBOnAryBegin, typename CBOnAryEnd, typename CBOnNull, typename CBOnIntegral, typename CBOnReal, typename CBOnString, typename CBOnBoolean>
+			state_in_array_t::~state_in_array_t( ) { }
+
+			template<typename CBOnObjBegin, typename CBOnAryBegin, typename CBOnAryEnd, typename CBOnNull, typename CBOnIntegral, typename CBOnReal, typename CBOnString, typename CBOnBoolean>
+			auto make_state_in_array_t( CBOnObjBegin cb_on_object_begin, CBOnAryBegin cb_on_array_begin, CBOnAryEnd cb_on_array_end, CBOnNull cb_on_null, CBOnIntegral cb_on_integral, CBOnReal cb_on_real, CBOnString cb_on_string, CBOnBoolean cb_on_boolean ) {
+				return state_in_array_t<
+							CBOnObjBegin, 
+							CBOnAryBegin, 
+							CBOnAryEnd, 
+							CBOnNull, 
+							CBOnIntegral, 
+							CBOnReal, 
+							CBOnString, 
+							CBOnBoolean>{ std::move( cb_on_object_begin ), 
+									std::move( cb_on_array_begin ), 
+									std::move( cb_on_array_end ), 
+									std::move( cb_on_null ), 
+									std::move( cb_on_integral ),
+									std::move( cb_on_real ),
+									std::move( cb_on_string ),
+									std::move( cb_on_boolean };
+			}
+
+			template<typename CBOnObjBegin, typename CBOnAryBegin>
 			struct state_none_t: public state_t {
-				state_none_t( ) = default;
+				CBOnObjBegin m_cb_on_object_begin;
+				CBOnAryBegin m_cb_on_array_begin;
+
+				state_none_t( ):
+						state_t{ },
+						m_cb_on_object_begin{ }
+						m_cb_on_array_begin{ } { }
+
+				state_none_t( CBOnObjBegin cb_on_object_begin, CBOnAryBegin cb_on_array_begin ):
+						state_t{ },
+						m_cb_on_object_begin{ std::move( cb_on_object_begin ) }
+						m_cb_on_array_begin{ std::move( cb_on_array_begin ) } { }
+
 				state_none_t( state_none_t const & ) = default;
 				state_none_t( state_none_t && ) = default;
 				state_none_t & operator=( state_none_t const & ) = default;
@@ -118,39 +298,17 @@ namespace daw {
 
 				~state_none_t( );
 
-				std::string to_string( ) const override;
 				void on_object_begin( ) override;
 				void on_array_begin( ) override;
-				void on_null( ) override;
-				void on_integral( boost::string_view value ) override;
-				void on_real( boost::string_view value ) override;
-				void on_string( boost::string_view value ) override;
-				void on_boolean( bool value ) override;
 			};	// state_none_t
 
-			state_t * get_state_fn( current_state_t s ) noexcept;
-			std::vector<state_t*> & state_stack( );
-			state_t & current_state( );
-			void push_and_set_next_state( current_state_t s );
-			void set_next_state( current_state_t s );
-			void pop_state( );
-			state_t * get_state_fn( current_state_t s ) noexcept;
+			template<typename CBOnObjBegin, typename CBOnAryBegin>
+			state_none_t::~state_none_t( ) { }
 
-			struct state_control_t {
-				std::string buffer;
-				state_t const & current_state( ) const;
-				void push( char c );
-				void clear_buffer( );
-				void on_object_begin( ) const;
-				void on_object_end( ) const;
-				void on_array_begin( ) const;
-				void on_array_end( ) const;				
-				void on_string( boost::string_view value ) const; 
-				void on_integral( boost::string_view value ) const;
-				void on_real( boost::string_view value ) const; 
-				void on_boolean( bool value ) const; 
-				void on_null( ) const; 
-			};
+			template<typename CBOnObjBegin, typename CBOnAryBegin>
+			auto make_state_none_t( CBOnObjBegin cb_on_object_begin, CB_on_array_begin ) {
+				return state_none_t<CBOnObjBegin, CBOnAryBegin>{ std::move( cb_on_object_begin ), std::move( cb_on_array_begin ) };
+			}
 		}	// namspace state
 	}	// namespace json
 }    // namespace daw
